d88fd7ce4db11f732dc9fcb1ed896a92e9eb4eb3
aaraamuthan <aaraamuthanb@gmail.com>
2020-08-07 08:54:57 +0530

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -55,9 +55,11 @@ class DishDetail extends Component {
     const dishItem = this.renderDish(dish);
     const commentItem = this.renderComments(dish.comments);
     return (
-      <div className="row">
-        {dishItem}
-        {commentItem}
+      <div className="container">
+        <div className="row">
+          {dishItem}
+          {commentItem}
+        </div>
       </div>
     );
   }
